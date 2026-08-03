5675152794e4a28802f4290bd9130e9c243e9584
Dhiren9939 <dhiren.nmf.1@gmail.com>
2026-08-03 22:05:45 +0530

    fix-infra: set destination cidr_blocks

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -111,6 +111,7 @@ resource "aws_security_group_rule" "ec2_to_internet" {
   protocol = "tcp"
   from_port = 443
   to_port = 443
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "rds_sg" {
