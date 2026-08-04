04577e6f9dca54ba88e980f414ce8c379d743734
Dhiren9939 <dhiren.nmf.1@gmail.com>
2026-08-04 21:27:32 +0530

    fix: remove cors support

--- backend/src/main/java/me/dhiren9939/mint/config/SecurityConfig.java
+++ backend/src/main/java/me/dhiren9939/mint/config/SecurityConfig.java
@@ -23,19 +23,19 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    @Profile("!prod")
-    public CorsConfigurationSource corsConfigurationSource(@Value("${allowed.cors.origin}") String allowedOrigin) {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(allowedOrigin));
-        config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
-        return source;
-    }
+//    @Bean
+//    @Profile("!prod")
+//    public CorsConfigurationSource corsConfigurationSource(@Value("${allowed.cors.origin:}") String allowedOrigin) {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true); 
+//        config.setAllowedOrigins(List.of(allowedOrigin));
+//        config.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
+//        config.setAllowedHeaders(List.of("*"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/api/**", config);
+//        return source;
+//    }
 
     @Bean
     public RateLimitFilter rateLimitFilter(ProxyManager<String> proxyManager,
