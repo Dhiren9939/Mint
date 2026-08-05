b5724e1583fbc23526172df058b91339d1cdb35e
Dhiren9939 <dhiren.nmf.1@gmail.com>
2026-08-05 13:36:09 +0530

    cd: preserver env

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 sudo cp /tmp/docker-compose.prod.yml /tmp/application.properties /tmp/application-prod.properties /opt/mint-backend/config
 
-sudo docker compose -f /opt/mint-backend/config/docker-compose.prod.yml down
+sudo -E docker compose -f /opt/mint-backend/config/docker-compose.prod.yml down
 
-sudo docker compose -f /opt/mint-backend/config/docker-compose.prod.yml up -d --pull always --remove-orphans
+sudo -E docker compose -f /opt/mint-backend/config/docker-compose.prod.yml up -d --pull always --remove-orphans
 
 yes | sudo docker image prune
