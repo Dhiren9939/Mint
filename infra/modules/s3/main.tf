5907881ac1e6a7394b6426e090072f7e44289f57
Dhiren9939 <dhiren.nmf.1@gmail.com>
2026-08-05 09:30:19 +0530

    infra: enable force destory on s3

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = var.mint_frontend
+  force_destroy = true
 }
 
 resource "aws_s3_bucket" "user_files" {
   bucket = var.mint_user_files
+  force_destroy = true
 }
