d809092359774acf0287a3329855436874bf9f5a
Dhiren9939 <dhiren.nmf.1@gmail.com>
2026-08-06 10:41:54 +0530

    cd: change redeploy.sh

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 sudo cp /tmp/docker-compose.prod.yml /tmp/application.properties /tmp/application-prod.properties /opt/mint-backend/config
 
-sudo -E docker compose -f /opt/mint-backend/config/docker-compose.prod.yml down
+sudo -E docker compose -f /opt/mint-backend/config/docker-compose.prod.yml up -d --pull --remove-orphans
 
-sudo -E docker compose -f /opt/mint-backend/config/docker-compose.prod.yml up -d --pull always --remove-orphans
-
-yes | sudo docker image prune
+sudo docker image prune -f
